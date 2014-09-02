c8db5c53819b0baee08eb75f4a8bdc1087197316
midipix <writeonce@midipix.org>
2014-09-01 20:54:05 -0400

    added 32-bit atomics (ntapi)

--- src/internal/headers/ntapi/bits/x86_64/nt_atomic_x86_64_asm__msvc.h
+++ src/internal/headers/ntapi/bits/x86_64/nt_atomic_x86_64_asm__msvc.h
@@ -5,6 +5,56 @@ int64_t	_InterlockedDecrement64(int64_t volatile * ptr);
 int64_t	_InterlockedExchangeAdd64(int64_t volatile * ptr, int64_t val);
 int64_t	_InterlockedCompareExchange64(int64_t volatile * dst, int64_t xchg, int64_t cmp);
 
+static __inline__ void at_locked_inc(
+	intptr_t volatile * ptr)
+{
+	_InterlockedIncrement64(ptr);
+	return;
+}
+
+
+static __inline__ void at_locked_dec(
+	intptr_t volatile * ptr)
+{
+	_InterlockedDecrement64(ptr);
+	return;
+}
+
+
+static __inline__ void at_locked_add(
+	intptr_t volatile *	ptr,
+	intptr_t		val)
+{
+	_InterlockedExchangeAdd64(ptr, val);
+	return;
+}
+
+
+static __inline__ void at_locked_sub(
+	intptr_t volatile *	ptr,
+	intptr_t		val)
+{
+	_InterlockedExchangeAdd64(ptr, -val);
+	return;
+}
+
+
+static __inline__ intptr_t at_locked_xadd(
+	intptr_t volatile *	ptr,
+	intptr_t		val)
+{
+	return _InterlockedExchangeAdd64(ptr, val);
+}
+
+
+static __inline__ intptr_t at_locked_xsub(
+	intptr_t volatile *	ptr,
+	intptr_t		val)
+{
+	return _InterlockedExchangeAdd64(ptr, -val);
+}
+
+
 static __inline__ intptr_t at_locked_cas(
 	intptr_t volatile *	dst,
 	intptr_t		cmp,
--- src/internal/headers/ntapi/nt_atomic.h
+++ src/internal/headers/ntapi/nt_atomic.h
@@ -3,6 +3,32 @@
 
 #include <psxtypes/psxtypes.h>
 
+static __inline__ void at_locked_inc(
+	intptr_t volatile * ptr);
+
+
+static __inline__ void at_locked_dec(
+	intptr_t volatile * ptr);
+
+
+static __inline__ void at_locked_add(
+	intptr_t volatile *	ptr,
+	intptr_t		val);
+
+
+static __inline__ void at_locked_sub(
+	intptr_t volatile *	ptr,
+	intptr_t		val);
+
+
+static __inline__ intptr_t at_locked_xadd(
+	intptr_t volatile *	ptr,
+	intptr_t		val);
+
+static __inline__ intptr_t at_locked_xsub(
+	intptr_t volatile *	ptr,
+	intptr_t		val);
+
 static __inline__ intptr_t at_locked_cas(
 	intptr_t volatile *	dst,
 	intptr_t		cmp,
