a92d1e19677c498d1a0347e642a0d75ed88a9e0d
midipix <writeonce@midipix.org>
2014-09-06 10:35:18 -0400

    initial draft of waio_read and waio_write

--- src/waio_read_write.c
+++ src/waio_read_write.c
@@ -1,1 +1,68 @@
-/* following the semantics of aio_read, aio_write */
+/*****************************************************************************/
+/*                                                                           */
+/*  Wild & Asynchronous I/O Library: implemented for the digital humanities  */
+/*                                                                           */
+/*  Copyright (C) 2014 Zvi Gilboa                                            */
+/*                                                                           */
+/*  This program is free software: you can redistribute it and/or modify     */
+/*  it under the terms of the GNU General Public License as published by     */
+/*  the Free Software Foundation, either version 3 of the License, or        */
+/*  (at your option) any later version.                                      */
+/*                                                                           */
+/*  This program is distributed in the hope that it will be useful,          */
+/*  but WITHOUT ANY WARRANTY; without even the implied warranty of           */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            */
+/*  GNU General Public License for more details.                             */
+/*                                                                           */
+/*  You should have received a copy of the GNU General Public License        */
+/*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    */
+/*                                                                           */
+/*****************************************************************************/
+
+
+#include <psxtypes/psxtypes.h>
+#include <ntapi/ntapi.h>
+#include <waio/waio.h>
+#include <waio/waio__llapi.h>
+#include "waio_impl.h"
+#include "waio_cx.h"
+#include "waio_errno.h"
+
+static int waio_read_write(
+	_in_	waio_cx			cx,
+	_in_	struct waio_aiocb *	aiocb,
+	_in_	int			lio_opcode)
+{
+	int32_t status;
+
+	status = waio_submit_single_request(
+		cx->paio,
+		aiocb,
+		lio_opcode,
+		__winapi->get_current_process_id(),
+		__winapi->get_current_thread_id());
+
+	if (status == NT_STATUS_INVALID_HANDLE)
+		return -EBADF;
+	else if (status)
+		return -EAGAIN;
+	return 0;
+}
+
+
+waio_api
+int waio_read(
+	_in_	waio_cx			cx,
+	_in_	struct waio_aiocb *	aiocb)
+{
+	return waio_read_write(cx,aiocb,WAIO_READ);
+}
+
+
+waio_api
+int waio_write(
+	_in_	waio_cx			cx,
+	_in_	struct waio_aiocb *	aiocb)
+{
+	return waio_read_write(cx,aiocb,WAIO_WRITE);
+}
